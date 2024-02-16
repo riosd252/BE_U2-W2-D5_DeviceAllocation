9d2c99fa01c05fb21641ece7510b9b652ba2292c
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-16 16:58:00 +0100

    Fixes a bug that prevented devices from being saved to DB.

--- src/main/java/DavidRios/DeviceAllocation/DTOs/DevicePayload.java
+++ src/main/java/DavidRios/DeviceAllocation/DTOs/DevicePayload.java
@@ -2,14 +2,11 @@ package DavidRios.DeviceAllocation.DTOs;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 
 public record DevicePayload(
-        @NotEmpty(message = "Field is mandatory.")
         @Min(value = 1)
         @Max(value = 3)
         int deviceType,
-        @NotEmpty(message = "Field is mandatory.")
         @Min(value = 1)
         @Max(value = 4)
         int deviceStatus
